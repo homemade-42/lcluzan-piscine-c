9dceffee4ef451b99948394f8446112ad7e49e08
Nouille Bobby <nouillebobby@MacBook-Pro-de-Charles.local>
2024-01-17 12:32:48 +0100

    modif Day01 ex02

--- day01/ex02/ft_print_reverse_alphabet.c
+++ day01/ex02/ft_print_reverse_alphabet.c
@@ -6,7 +6,7 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 12:25:36 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/01/17 12:30:02 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/01/17 12:31:47 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,4 @@
 void	ft_print_reverse_alphabet(void)
 {
 	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
-}
-
-int	main(void)
-{
-	ft_print_reverse_alphabet();
-	return (0);
 }
