ca0806c18e9223a2e56d0df50ac3ae4246ab6b45
Nouille Bobby <nouillebobby@eduroam-prg-gm-1-1-168.net.univ-paris-diderot.fr>
2024-01-18 15:13:17 +0100

    modif day00 ex05

--- day01/ex05/ft_print_comb.c
+++ day01/ex05/ft_print_comb.c
@@ -6,7 +6,7 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 16:03:39 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/01/18 15:06:38 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/01/18 15:12:20 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	ft_print_number(int a, int b, int c)
 {
 	ft_putchar(a + '0');
 	ft_putchar(b + '0');
-	ft_putchar(c + '0';
+	ft_putchar(c + '0');
 }
 
 void	ft_print_comb(void)
