6b1a5241fbc914fb20a04db0c211a2e2b5e06284
Nouille Bobby <nouillebobby@eduroam-prg-gm-1-1-196.net.univ-paris-diderot.fr>
2024-01-25 16:56:53 +0100

    fix day03 ex01

--- day03/ex01/ft_strncpy.c
+++ day03/ex01/ft_strncpy.c
@@ -6,11 +6,11 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:29:37 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/01/25 16:42:50 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/01/25 16:56:25 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strncpy(char *dest, char *src, unsigned int n)
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int	i;
 
