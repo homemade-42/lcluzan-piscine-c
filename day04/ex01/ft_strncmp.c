8935e4eac0acd349cc5ff7b5bc48d1b6b986f979
Nouille Bobby <nouillebobby@eduroam-prg-gm-1-0-254.net.univ-paris-diderot.fr>
2024-02-01 15:25:48 +0100

    fix2 ex01

--- day04/ex01/ft_strncmp.c
+++ day04/ex01/ft_strncmp.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/31 15:56:44 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/02/01 14:59:27 by lcluzan          ###   ########.fr       */
+/*   Created: 2024/02/01 15:24:57 by lcluzan           #+#    #+#             */
+/*   Updated: 2024/02/01 15:25:00 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 		return (0);
 	while (s1[i] == s2[i] && (i < n - 1) && s1[i])
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
