443dacbe6bfd41c3bc386c860aa5dc4b73308d0b
Nouille Bobby <nouillebobby@eduroam-prg-gm-1-0-205.net.univ-paris-diderot.fr>
2024-02-08 17:29:31 +0100

    fix ex01

--- day05/ex01/ft_putstr.c
+++ day05/ex01/ft_putstr.c
@@ -6,20 +6,23 @@
 /*   By: lcluzan <lcluzan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 16:55:00 by lcluzan           #+#    #+#             */
-/*   Updated: 2024/02/08 17:03:40 by lcluzan          ###   ########.fr       */
+/*   Updated: 2024/02/08 17:28:53 by lcluzan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char	*str)
+int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
-		write(1, &str[i], 1);
+	while (str[i])
 		i++;
-	}
+	return (i);
+}
+
+void	ft_putstr(char	*str)
+{
+	write(1, str, ft_strlen(str));
 }
